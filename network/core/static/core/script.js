b0855711a02c7ec6dde1dfe391168c2e7914fce5
zbrown11-17 <92639084+zbrown11-17@users.noreply.github.com>
2023-04-10 20:37:33 -0400

    Better accessibility and profile menu

--- network/core/static/core/script.js
+++ network/core/static/core/script.js
@@ -2,15 +2,13 @@ let loggedInBarUser = null
 
 document.addEventListener('DOMContentLoaded', function() {
 
-    const nav = document.querySelector('nav')
-    nav.append(baseNavbar())
-
     const check = document.getElementById("user-menu");
     if (check !== null) {
         loggedInBarUser = parseInt(check.dataset.user)
     };
 
-    //baseNavbar();
+    const nav = document.querySelector('nav')
+    nav.append(baseNavbar())
 
 });
 
@@ -19,7 +17,12 @@ function baseNavbar() {
     const navBar = document.createElement("div");
     navBar.setAttribute("class", "nav-div");
 
+
+    const unOrLi = document.createElement("ul");
+    navBar.append(unOrLi);
+
     // Brand Div
+    const brandLi = document.createElement("li");
     const brand = document.createElement("div");
     brand.setAttribute("class", "nb-brand");
     const brandLink = document.createElement("a");
@@ -31,7 +34,8 @@ function baseNavbar() {
     brandName.innerText = "Jukework";
     brandLink.append(brandIcon, brandName);
     brand.append(brandLink);
-    navBar.append(brand);
+    brandLi.append(brand);
+    unOrLi.append(brandLi);
 
 
     //const unOrLi = document.createElement("ul");
@@ -39,18 +43,24 @@ function baseNavbar() {
 
     // Dropdown
 
+    const gDdLi = document.createElement("li");
     const genreDropdownDiv = genreDropdownMenu();
-    navBar.append(genreDropdownDiv);
+    gDdLi.append(genreDropdownDiv);
+    unOrLi.append(gDdLi);
 
     // Check if user is logged in
     const check = document.getElementById("user-menu");
     if (check !== null) {
+        const pDdLi = document.createElement("li");
+        pDdLi.setAttribute("id", "nav-right");
         const profileDropdownDiv = profileDropdownMenu();
-        navBar.append(profileDropdownDiv);
+        pDdLi.append(profileDropdownDiv);
+        unOrLi.append(pDdLi);
     } else {
         // Make a sign in button
-        const signInDiv = document.createElement("div");
-        signInDiv.setAttribute("class", "nav-right");
+        const signLi = document.createElement("li");
+        //const signInDiv = document.createElement("div");
+        signLi.setAttribute("id", "nav-right");
         const signInLink = document.createElement("a");
         signInLink.setAttribute("href", `/users/login`);
         signInLink.setAttribute("class", "sign-in-link");
@@ -61,8 +71,9 @@ function baseNavbar() {
         signInText.setAttribute("class", "sign-in-text");
         signInText.innerText = "Log In"
         signInLink.append(signInIcon, signInText);
-        signInDiv.append(signInLink);
-        navBar.append(signInDiv);
+        //signInDiv.append(signInLink);
+        signLi.append(signInLink);
+        unOrLi.append(signLi);
     }
 
     return navBar;
@@ -81,12 +92,12 @@ function genreDropdownMenu() {
     };
 
     const dropdownDiv = document.createElement("div");
-    dropdownDiv.setAttribute("class", "gnr-dropdown");
+    dropdownDiv.setAttribute("class", "nav-dropdown");
 
     // Span button that once clicked will show dropdown
     const dropdownBtn = document.createElement("input");
     dropdownBtn.setAttribute("type", "button");
-    dropdownBtn.setAttribute("class", "genre-btn");
+    dropdownBtn.setAttribute("class", "drpdwn-btn");
     dropdownBtn.value = "Filter by Genres..."
     const dropdownIcon = document.createElement("i");
     dropdownIcon.setAttribute("class", "bx bx-chevron-down genre-arw");
@@ -100,7 +111,7 @@ function genreDropdownMenu() {
     Object.entries(genres).forEach(genre => {
 
         const genreLink = document.createElement("a");
-        genreLink.setAttribute("class", "gnr-dropdown-item");
+        genreLink.setAttribute("class", "nav-dropdown-item");
         genreLink.setAttribute("href", `posts/${genre[1]}`);
         genreLink.innerText = genre[0]
         dropdownCnt.append(genreLink)
@@ -108,6 +119,8 @@ function genreDropdownMenu() {
     })
     dropdownDiv.addEventListener("click", () => {
         dropdownCnt.style.display = "block";
+        dropdownBtn.style.backgroundColor = "var(--accent)";
+        dropdownDiv.style.backgroundColor = "var(--accent)";
     });
 
     // https://www.techiedelight.com/hide-div-click-outside-javascript/
@@ -116,6 +129,8 @@ function genreDropdownMenu() {
         var container = document.getElementById('gnr-dropdown-cnt');
         if (!container.contains(e.target)) {
             container.style.display = 'none';
+            dropdownBtn.style.backgroundColor = "";
+            dropdownDiv.style.backgroundColor = "";
         }
     });
 
@@ -130,16 +145,12 @@ function profileDropdownMenu() {
     dropdownDiv.setAttribute("class", "pro-dropdown");
 
     // Span button that once clicked will show dropdown
-    const dropdownSpan = document.createElement("span");
-    dropdownSpan.setAttribute("class", "profile-span");
     const dropdownBtn = document.createElement("input");
     dropdownBtn.setAttribute("type", "button");
-    dropdownBtn.setAttribute("class", "profile-btn");
-    dropdownBtn.value = "Uh Oh"
+    dropdownBtn.setAttribute("class", "drpdwn-btn");
     const dropdownIcon = document.createElement("i");
     dropdownIcon.setAttribute("class", "bx bx-chevron-down profile-arw");
-    dropdownSpan.append(dropdownBtn, dropdownIcon);
-    dropdownDiv.append(dropdownSpan);
+    dropdownDiv.append(dropdownBtn, dropdownIcon);
 
 
     const dropdownCnt = document.createElement("div");
@@ -147,31 +158,45 @@ function profileDropdownMenu() {
     dropdownDiv.append(dropdownCnt);
 
     const profileLink = document.createElement("a");
-    profileLink.setAttribute("class", "pro-dropdown-item");
+    profileLink.setAttribute("class", "nav-dropdown-item");
     profileLink.setAttribute("href", `#`);
-    profileLink.innerText = "Profile"
-    dropdownCnt.append(profileLink)
+    profileLink.innerText = "Profile";
+    dropdownCnt.append(profileLink);
 
     const followingLink = document.createElement("a");
-    followingLink.setAttribute("class", "pro-dropdown-item");
+    followingLink.setAttribute("class", "nav-dropdown-item");
     followingLink.setAttribute("href", `#`);
-    followingLink.innerText = "Profile"
-    dropdownCnt.append(followingLink)
+    followingLink.innerText = "Following";
+    dropdownCnt.append(followingLink);
 
     const settingsLink = document.createElement("a");
-    settingsLink.setAttribute("class", "pro-dropdown-item");
+    settingsLink.setAttribute("class", "nav-dropdown-item");
     settingsLink.setAttribute("href", `#`);
-    settingsLink.innerText = "Settings"
-    dropdownCnt.append(settingsLink)
+    settingsLink.innerText = "Settings";
+    dropdownCnt.append(settingsLink);
 
     const logoutLink = document.createElement("a");
-    logoutLink.setAttribute("class", "pro-dropdown-item");
+    logoutLink.setAttribute("class", "nav-dropdown-item");
     logoutLink.setAttribute("href", `/users/logout`);
-    logoutLink.innerText = "Logout"
-    dropdownCnt.append(logoutLink)
+    logoutLink.innerText = "Logout";
+    dropdownCnt.append(logoutLink);
 
-    dropdownSpan.addEventListener("click", () => {
+    // Get user information
+    fetch(`/users/${loggedInBarUser}`)
+    .then(response => response.json() )
+    .then(user => {
+
+        dropdownBtn.value = user.username;
+
+    })
+    .catch(error => {
+        console.log(error);
+    });
+
+    dropdownDiv.addEventListener("click", () => {
         dropdownCnt.style.display = "block";
+        dropdownBtn.style.backgroundColor = "var(--secondary)";
+        dropdownDiv.style.backgroundColor = "var(--secondary)";
     });
 
     // https://www.techiedelight.com/hide-div-click-outside-javascript/
@@ -180,6 +205,8 @@ function profileDropdownMenu() {
         var container = document.getElementById('pro-dropdown-cnt');
         if (!container.contains(e.target)) {
             container.style.display = 'none';
+            dropdownBtn.style.backgroundColor = "";
+            dropdownDiv.style.backgroundColor = "";
         }
     });
 
